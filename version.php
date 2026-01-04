1c3b41296ff371897fd9f56469585db0f6e4861b
SwiftLearn UK <david@swiftlearn.co.uk>
2026-01-04 01:00:58 +0000

    Remove Bootstrap 5 CDN from all templates to fix user menu
    - Remove bootstrap@5.3.0 CDN from wizard.mustache
    - Remove bootstrap@5.3.0 CDN from subscription.mustache
    - Remove bootstrap@5.3.0 CDN from assistant.mustache
    - Remove bootstrap@5.3.0 CDN from registration.mustache
    - Remove bootstrap@5.3.0 CDN from generated_reports.mustache
    - Moodle includes Bootstrap 4.x - loading Bootstrap 5 breaks navigation dropdowns
    - Added notes in templates to prevent re-adding Bootstrap CDN

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_adeptus_insights';
-$plugin->version   = 2026010305; // YYYYMMDDXX - Change wizard page layout to 'report' to fix Moodle reactive errors
+$plugin->version   = 2026010306; // YYYYMMDDXX - Remove Bootstrap 5 CDN to fix user menu dropdown conflict
 $plugin->requires  = 2022112800; // Moodle 4.1
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.35';
+$plugin->release   = '1.1.36';
