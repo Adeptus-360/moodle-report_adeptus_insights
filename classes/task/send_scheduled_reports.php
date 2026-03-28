67b8bd226cc394941a69e891928f036c84d4a17a
Bill Ion <bill@brainycheeks.com>
2026-03-28 22:51:27 +0000

    fix(CI): add missing file-level docblocks and mustache @copyright/@license tags
    - Add @copyright and @license to builder_report_view.mustache
    - Add @copyright and @license to builder_reports_list.mustache
    - Add file-level docblocks (with @package, @copyright, @license) to all
      PHP class files that were missing them:
      - classes/alert_engine.php
      - classes/external/*.php (27 files)
      - classes/form/schedule_form.php
      - classes/learner_dashboard.php
      - classes/pdf/scheduled_report_template.php
      - classes/report_executor.php
      - classes/task/build_analytics_base.php
      - classes/task/build_materialized_table.php
      - classes/task/send_alert_digest.php
      - classes/task/send_scheduled_reports.php
      - classes/teacher_metrics.php
      - classes/time_calculator.php
    
    Resolves phpcs, phpdoc, mustache lint, and validate CI failures.

--- classes/task/send_scheduled_reports.php
+++ classes/task/send_scheduled_reports.php
@@ -14,6 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Scheduled task to send scheduled reports via email.
+ *
+ * @package     report_adeptus_insights
+ * @copyright   2026 Adeptus 360 <info@adeptus360.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 namespace report_adeptus_insights\task;
 
 use core\lock\lock_config;
