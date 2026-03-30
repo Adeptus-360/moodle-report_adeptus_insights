79a708e83926007fa52ec6993a3f13779289b348
Kai Nakamura <kai@adeptus360.com>
2026-03-30 06:46:02 +0000

    release: bump version to 2026033001 for v2.0.0 release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_adeptus_insights';
-$plugin->version   = 2026031601;
+$plugin->version   = 2026033001;
 $plugin->requires  = 2022112800; // Moodle 4.1.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '2.0.0';
